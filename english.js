a31e903ccc661d5fd9d771da93bfcdb4a99f0158
Ishwaryakiran <ishwaryakiran363@gmail.com>
2025-02-25 10:02:57 +0530

    Add files via upload

---
+++ english.js
@@ -0,0 +1,7 @@
+function enrollCourse() {
+    alert("You have successfully enrolled in the English Language course!");
+}
+
+function goBack() {
+    window.location.href = "index.html";  // Change to the actual course list page
+}
